e4bf1a02f78f084cb7445668cdb52c6b7be41fec
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-02-16 01:55:21 -0500

    Added recordingInProgress button

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingInProgress: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,6 +23,7 @@ class ViewController: UIViewController {
 
 
     @IBAction func recordAudio(sender: UIButton) {
+        recordingInProgress.hidden = false
         print("Recording Audio")
     }
 }
